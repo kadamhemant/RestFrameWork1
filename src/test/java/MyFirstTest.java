a5fe1ffc4b4c391171e7330cb3d5196be603eece
Hemant.Kadam <kadamhemant53@gmail.com>
2018-07-05 14:41:56 -0400

    initial commit

---
+++ src/test/java/MyFirstTest.java
@@ -0,0 +1,15 @@
+import config.TestConfig;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.*;
+public class MyFirstTest extends TestConfig {
+    @Test
+    public void myFirstTest ()
+    {
+        given().
+                log().all().
+                when().get("videogames/1").
+                 then().
+        log().all();
+    }
+}
